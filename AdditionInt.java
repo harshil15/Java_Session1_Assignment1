b6005783b396977a50b2890b625733f584b8ea79
Harshil <harshil.chhajed12@gmail.com>
2016-04-24 10:30:51 +0530

    Assignment 1
    Addition of Int and Float number

---
+++ AdditionInt.java
@@ -0,0 +1,16 @@
+package acadgild;
+
+public class AdditionInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a; 
+		float b;
+		a=10;
+		b=15.5f;
+		int d = (int) b;
+		int c= a+d;
+		System.out.println("Sum of int and float Number  ::  " + c);
+	}
+
+}
